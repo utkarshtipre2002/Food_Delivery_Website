3b8b54c886078a228b043404931119c8f3fbdec6
utkarshtipre2002 <utkarshtipre2002@gmail.com>
2024-03-04 22:46:16 +0530

    first commit

--- backend/Routes/CreatUser.js
+++ backend/Routes/CreatUser.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
+const axios = require("axios");
 const jwt = require("jsonwebtoken");
 
 const bcrypt = require("bcryptjs");
@@ -100,5 +101,78 @@ router.post(
     }
   }
 );
+
+// router.post("/getlocation", async (req, res) => {
+//   try {
+//     let lat = req.body.latlong.lat;
+//     let long = req.body.latlong.long;
+//     console.log(lat, long);
+
+//     let location = await axios
+//       .get(
+//         "https://api.opencagedata.com/geocode/v1/json?q=" +
+//           lat +
+//           "+" +
+//           long +
+//           "&key=74c89b3be64946ac96d777d08b878d43"
+//       )
+//       .then((response) => {
+//         console.log(response.data.results);
+//         let result = response.data.results[0].components;
+//         console.log(result);
+
+//         let { village, county, state_district, state, postcode } = result;
+//         return String(
+//           village +
+//             "," +
+//             county +
+//             "," +
+//             state_district +
+//             "," +
+//             state +
+//             "\n" +
+//             postcode
+//         );
+//       });
+
+//     res.json({ location });
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).json({ error: "Internal Server Error" });
+//   }
+// });
+
+router.post("/getlocation", async (req, res) => {
+  try {
+    let lat = req.body.latlong.lat;
+    let long = req.body.latlong.long;
+    console.log(lat, long);
+
+    let response = await axios.get(
+      "https://api.opencagedata.com/geocode/v1/json?q=" +
+        lat +
+        "+" +
+        long +
+        "&key=74c89b3be64946ac96d777d08b878d43"
+    );
+
+    console.log(response.data); // Log the entire response to see its structure
+
+    let location = response.data.results[0]?.components;
+
+    if (!location) {
+      throw new Error("Location not found");
+    }
+
+    let { village, county, state_district, state, postcode } = location;
+
+    let formattedLocation = `${village},${county},${state_district},${state}\n${postcode}`;
+
+    res.json({ location: formattedLocation });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
